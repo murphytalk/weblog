e9a4d3c48ca4af0ffc2ec58a31f6b81927797fad
Mu Lu <murphytalk@gmail.com>
2016-07-14 22:40:14 +0900

    convert <img/> html to ReStructureText image

--- convert.sh
+++ convert.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
+
 for p in content/*.rst;do
+    #convert structureText hyper link to ReStructureText hyper link
     sed -E -i -e 's/"(.*)":(http:\/\/([a-z]|\.|[0-9]|[A-Z]|_|&|%|#|\/|-|=|\?|~)*)/ `\1 <\2>`_ /g'  $p
+    #convert <img/> html to ReStructureText image 
+    sed -E -i -e 's/<img *src="(\/.*)">/\n.. image:: {filename}\/images\1\n   :align: center\n/g' $p
+    sed -E -i -e 's/<img *src="(http:\/.*)">/\n.. image:: \1\n   :align: center\n/g' $p
 done
